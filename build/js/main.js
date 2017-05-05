12ba3bb74a3fc705bd5c2cc19c50b2f5a8c18640
im-sad <im-sad@ya.ru>
2017-05-05 09:26:59 +0300

    up

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,21 @@
 $(function(){
 
 
-	$('.no-click').on('click', function(e){
-		e.preventDefault();
+	//MODAL
+	$('.js-pop, [data-form]').magnificPopup({
+		type: 'inline',
+		midClick: true,
+		closeBtnInside: true,
+		closeMarkup: '<button type="button" class="mfp-close mfp-new-close"><svg><use xlink:href="#icn-cross"/></svg></button>',
+		alignTop: false,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom',
 	});
+
+	$(document).on('click', '.mfp-close', function (e) {
+			e.preventDefault();
+			$.magnificPopup.close();
+	});
+
 
 });
--- src/js/partials/core.js
+++ src/js/partials/core.js
@@ -1,8 +1,24 @@
 $(function(){
 
 
-	$('.no-click').on('click', function(e){
-		e.preventDefault();
+	//MODAL
+	$('.js-pop, [data-form]').magnificPopup({
+		type: 'inline',
+		midClick: true,
+		closeBtnInside: true,
+		closeMarkup: '<button type="button" class="mfp-close mfp-new-close"><svg><use xlink:href="#icn-cross"/></svg></button>',
+		alignTop: false,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom',
 	});
+
+	$(document).on('click', '.mfp-close', function (e) {
+			e.preventDefault();
+			$.magnificPopup.close();
+	});
+
+
+	//TEL
+	$('.js-tel, input[type="tel"]').mask("+7(999) 999-99-99", {placeholder:"+7(___) ___-__-__"});
 
 });
